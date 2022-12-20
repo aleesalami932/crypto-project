d3944f0cc37f61e925a9f27b466222cbddff29bc
Ali Salami <asalami932@gmail.com>
2022-12-20 12:55:51 +0200

    optimization

--- packages/frontend/src/features/counter/Counter.tsx
+++ packages/frontend/src/features/counter/Counter.tsx
@@ -2,12 +2,19 @@ import React, { useState } from "react";
 import aesDecrypt from "./aes-decrypt";
 import aesEncrypt from "./aes-encrypt";
 import hashingFunction from "./hash";
+
 export function Counter() {
   const [plainText, setPlainText] = useState<string>("");
   const [hashedText, setHashedText] = useState<string>("");
   const [aesCypheredText, setAesCypheredText] = useState<any>();
   const [aesDecryptedText, setAesDecryptedText] = useState<any>();
   const key_128 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+  function clearData() {
+    setPlainText("");
+    setHashedText("");
+    setAesCypheredText("");
+    setAesDecryptedText("");
+  }
   return (
     <div>
       <input
@@ -17,8 +24,6 @@ export function Counter() {
           setPlainText(inputVal);
           const hashedText = hashingFunction(inputVal);
           setHashedText(hashedText);
-          const encryptedByAes = aesEncrypt(plainText, key_128);
-          setAesCypheredText(encryptedByAes);
         }}
         placeholder={"plain text"}
       />
@@ -28,9 +33,16 @@ export function Counter() {
       <p style={{ textAlign: "start", paddingLeft: "10px" }}>
         my hashed text is: {hashedText}
       </p>
-      <p style={{ textAlign: "start", paddingLeft: "10px" }}>
-        my aes cyphered text is: {aesCypheredText}
-      </p>
+
+      <button
+        style={{ margin: "10px", padding: "10px" }}
+        onClick={() => {
+          const encryptedByAes = aesEncrypt(plainText, key_128);
+          setAesCypheredText(encryptedByAes);
+        }}
+      >
+        encrypt
+      </button>
       <button
         style={{ margin: "10px", padding: "10px" }}
         onClick={() => {
@@ -40,6 +52,17 @@ export function Counter() {
       >
         decrypt
       </button>
+      <button
+        style={{ margin: "10px", padding: "10px" }}
+        onClick={() => {
+          clearData();
+        }}
+      >
+        clear
+      </button>
+      <p style={{ textAlign: "start", paddingLeft: "10px" }}>
+        my aes cyphered text is: {aesCypheredText}
+      </p>
       <p style={{ textAlign: "start", paddingLeft: "10px" }}>
         my aes decrypted text is: {aesDecryptedText}
       </p>
